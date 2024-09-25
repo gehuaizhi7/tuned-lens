924537b95675e90f49e0c03ecfa62cac55e7f355
Huaizhi Ge <60053346+gehuaizhi7@users.noreply.github.com>
2024-09-25 15:55:55 -0400

    Update model_surgery.py

--- tuned_lens/model_surgery.py
+++ tuned_lens/model_surgery.py
@@ -121,8 +121,8 @@ def get_final_norm(model: Model) -> Norm:
         final_layer_norm = base_model.ln_f
     elif isinstance(base_model, models.llama.modeling_llama.LlamaModel):
         final_layer_norm = base_model.norm
-    elif isinstance(base_model, models.gemma.modeling_gemma.GemmaModel):
-        final_layer_norm = base_model.norm
+    # elif isinstance(base_model, models.gemma.modeling_gemma.GemmaModel):
+    #     final_layer_norm = base_model.norm
     elif isinstance(base_model, PeftModelForCausalLM):
     # Access the base model wrapped by the PeftModelForCausalLM
         final_layer_norm = base_model.base_model.model.norm if hasattr(base_model.base_model.model, 'norm') else base_model.base_model.model.ln_f
